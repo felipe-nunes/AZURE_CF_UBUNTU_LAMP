e8728178a8fcda4ae4cc4cfe0fe7d21c021a50a8
Felipe Nunes <felipe-nunes@users.noreply.github.com>
2017-01-27 19:36:15 -0200

    Update install_lamp.sh

--- install_lamp.sh
+++ install_lamp.sh
@@ -8,7 +8,4 @@ apt-get -y update
 
 # install the LAMP stack
 apt-get -y install git curl 
-apt-get -y install apache2 
-apt-get -y install php7.0 php7.0-common php7.0-curl php7.0-mysql php7.0-json php7.0-cgi php7.0-cli php7.0-odbc php7.0-sqlite3 php7.0-mcrypt php7.0-xml php7.0-soap php7.0-xmlrpc php7.0-gd php7.0-mbstring
-apt-get -y install libapache2-mod-php7.0 
-apt-get -y install mysql-server mysql-utilities mysql-common mysql-client libmysqlclient20
+
