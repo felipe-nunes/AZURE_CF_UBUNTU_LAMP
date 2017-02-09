c72c942641ccf025d4f844ab14ccd92e4c44f6b1
Felipe Nunes <felipe-nunes@users.noreply.github.com>
2017-02-09 02:15:00 -0200

    Update install_lamp.sh

--- install_lamp.sh
+++ install_lamp.sh
@@ -16,3 +16,7 @@ apt-get -y install php7.0 libapache2-mod-php7.0
 # php --ini
 sudo bash -c 'echo "<?php phpinfo(); ?>" > /var/www/html/infophp.php'
 
+apt-get -y install php7.0-common php7.0-curl php7.0-mysql php7.0-json php7.0-cgi php7.0-cli php7.0-odbc php7.0-sqlite3 php7.0-mcrypt php7.0-xml php7.0-soap php7.0-xmlrpc php7.0-gd php7.0-mbstring
+
+
+
